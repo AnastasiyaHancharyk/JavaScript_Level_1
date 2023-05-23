e1836b2a54371d6b4b4409a734c437aa2937092d
Anastasiya <nastya.goncharik97@gmail.com>
2023-05-24 01:20:42 +0300

    New

--- JavaScript Level 1/10. callback functions.js
+++ JavaScript Level 1/10. callback functions.js
@@ -1,1 +1,29 @@
-// Callback - это функция, которая должна быть выполнена после того, как другая функция завершила выполнение (то есть позволяет нам быть уверенными в том, что определенный код не начнет исполнение до того момента, пока другой код не завершит исполнение). 
+// Callback - функция, переданная в другую функцию в качестве аргумента, которая затем вызывается по завершению какого-либо действия. 
+// Например:
+function example (x, y_callback){
+    console.log(x);
+    y_callback();
+}
+function callback () {
+    console.log("Callback Function");
+}
+example("Some text", callback);
+
+// Callback функции позволяют "откладывать" длительные операции и вместо ожидания их завершения выполнять другой код.
+
+function studying (start, end) {
+    console.log(start);
+    setTimeout(end, 5000);
+}
+function process () {
+    console.log("The class is in progress");
+}
+
+function stop () {
+    console.log("The class has ended");
+}
+
+studying("The class has begun", stop);
+process();
+
+// Сообщения "The class has begun" и "The class is in progress" отобразятся в консоли одновременно, а "The class has ended" - спустя указанное время.
