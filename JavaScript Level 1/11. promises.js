68f1f05a80c73755dd7ccaadb1ed0fa22696d457
Anastasiya <nastya.goncharik97@gmail.com>
2023-05-30 23:05:16 +0300

    New

--- JavaScript Level 1/11. promises.js
+++ JavaScript Level 1/11. promises.js
@@ -1,1 +1,33 @@
-// Promises - это объект, представляющий результат успешного или неудачного завершения асинхронной операции.
+// Promises - это объект, представляющий результат успешного или неудачного завершения асинхронной операции. То есть это может быть resolve, если функция прошла успешно или reject, если функция вернула ошибку.
+/* У промиса есть три состояния:
+    - Промис в состоянии ожидания (pending).
+    - Промис решен (resolved).
+    - Промис отклонен (rejected).*/
+// Синтаксис промисов выглядит следующим образом: new Promise(function (resolve, reject) { … } );
+
+let is_the_weather_wonderful = true;
+let answer_the_question = new Promise(  // Создание промиса
+  function (resolve, reject) {
+    if(is_the_weather_wonderful) {
+      resolve("Yes, but I always carry an umbrella.")
+    } else {
+      reject("For the season, it's been unusually rainy.")
+    }
+  }
+);
+
+let ask_the_question = function () {  // Вызов промиса
+  answer_the_question
+  .then(function (fulfilled) {
+    console.log(fulfilled);
+  })
+  .catch(function (error){
+    console.log(error);
+  });
+};
+
+ask_the_question();  // Если is_the_weather_wonderful = true, то в консоль будет выведено "Yes, but I always carry an umbrella.", если false, то - "For the season, it's been unusually rainy.".
+
+
+// then — запускает колбек, который вы передали, когда промис завершен.
+// catch — запускает колбек, который вы передали, когда что-то идет не так, что вызывает reject вместо resolve.
