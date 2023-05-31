623aacb79ef456c3628d7849dd0e8d7e0df60d26
Anastasiya <nastya.goncharik97@gmail.com>
2023-05-31 18:07:12 +0300

    New

--- Node/1. file operations (create, find, delete file, save information into the file).js
+++ Node/1. file operations (create, find, delete file, save information into the file).js
@@ -1,15 +1,15 @@
 // fs (FileSystem) - модуль, дающий средства для работы с файловой системой
 
-// Create
-function create_file() {
-    FileSystem.open("testFile.txt", "w", function(err) {
-        if(err) throw err;
-        console.log("File created");
+// Create operations
+   // 1. fs.open():
+   let fs = require("fs");
+   fs.open("New File.txt", "r", function (err) {
+      if (err) throw err;
+      console.log("Done!");
     });
 
-}
 /* - fs.open/FileSystem.open используется для создания нового файла;
-   - Первый аргумент ("testFile.txt") - это имя файла;
+   - Первый аргумент ("New File.txt") - это имя файла;
    - Второй аргумент (флаг "w") указывает, что именно нужно сделать с файлом. В данном случае - открыть файл для записи (writing);
    - err передает ошибки, которые могут возникнуть при создании файла.
 
@@ -23,6 +23,20 @@ function create_file() {
 */
 
 
+   // 2. fs.appendFile() - добавляет указанное значение в файл. Если же файл не существует, он будет создан:
+   let fs = require("fs");
+   fs.appendFile("New File.txt", "Red Cat :)", function(err) {
+    if (err) throw err;
+    console.log("Done!");
+   });                     // В созданный ранее файл будет добавлена строчка "Red Cat :)"
+
+
+   // 3. fs.writeFile() - заменяет значение в указанном файле, а также создает новый файл, если его не существует:
+   let fs = require("fs");
+   fs.writeFile("New File.txt", "Orange Kitten Simba", function(err) {
+    if (err) throw err;
+    console.log("Done!");
+   });                     // В созданном ранее файле старое значение "Red Cat :)" будет изменено на указанное выше - "Orange Kitten Simba".
 
 
 // Find
